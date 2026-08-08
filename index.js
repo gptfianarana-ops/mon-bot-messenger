968052fb0483bb1827df70fef25386fe14ab6bc7
gptfianarana-ops <gptfianarana@gmail.com>
2026-08-08 22:02:30 +0200

    Fix module import to include file extension

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { processExamRequests } = require('./examResultsModule');
+const { processExamRequests } = require('./examResultsModule.js'); 
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
